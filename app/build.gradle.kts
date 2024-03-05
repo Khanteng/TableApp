3c7357d815dec0f9093548a42a9b171d59a3a7dc
Khanteng <nurzhanyeskendir@yandex.ru>
2024-03-05 17:42:33 +0500

    upadete fireBase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
+
 
 android {
     namespace = "com.example.tableapp"
@@ -33,6 +35,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -41,6 +46,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
+    implementation("com.google.firebase:firebase-database:20.3.1")
+    implementation("com.google.firebase:firebase-storage:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/tableapp/MainActivity.kt
+++ app/src/main/java/com/example/tableapp/MainActivity.kt
@@ -2,10 +2,78 @@ package com.example.tableapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.annotation.NavigationRes
+import androidx.annotation.StringRes
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var toolbarLayout: Toolbar
+    lateinit var navView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+        init()
+
+    }
+    private fun initViews(){
+        drawerLayout = findViewById(R.id.drawerLayout)
+        toolbarLayout = findViewById(R.id.toolbar)
+        navView = findViewById(R.id.navView)
+    }
+
+    private fun init(){
+        val toggle = ActionBarDrawerToggle(this,drawerLayout,toolbarLayout,
+            R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        when(item.itemId){
+
+            R.id.id_my_ads -> {
+                Toast.makeText(this,"qwdqwdwqd",Toast.LENGTH_LONG)
+            }
+            R.id.id_car -> {
+
+            }
+            R.id.id_pc -> {
+
+            }
+            R.id.id_smart -> {
+
+            }
+            R.id.id_dm -> {
+
+            }
+            R.id.ac_sign_up -> {
+
+            }
+            R.id.ac_sign_in -> {
+
+            }
+            R.id.ac_sign_out -> {
+
+            }
+
+
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
